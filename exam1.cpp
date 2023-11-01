fd51c36462a132ee12ff613b10c3bb85d76dd77c
이안 <145183497+2anizirong@users.noreply.github.com>
2023-11-01 15:21:15 +0900

    1주차

---
+++ exam1.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//	int math = 0, korean = 0;
+//	int sum = 0;		//초기화를 안해도 괜찮다.
+//
+//	cout << "수학 : ";
+//	cin >> math;
+//
+//	cout << "국어 : ";
+//	cin >> korean;
+//
+//	sum = math + korean;
+//	
+//	cout << sum << endl;
+//
+//	if (sum > 180) {
+//		cout << "참 잘했어요" << endl;
+//	}
+//	else {
+//		cout << "좀 더 노력하세요" << endl;
+//	}
+//
+//	return 0;
+//}
---
+++ exam2.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 1;
+//	int sum = 0;
+//
+//	while (a <= 10) 
+//	{
+//		//cout << a << endl;	//1~10까지의 수를 출력하기
+//		//a += 1;
+//
+//		sum += a;
+//		a++;
+//	}
+//	cout << sum << endl;
+//
+//	return 0;
+//}
